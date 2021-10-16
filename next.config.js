4863655ebb9b244665cb2e8c54cf7200b93acfb9
Shreyansh Rajput <shreyansh.singhrajput2019@gmail.com>
2021-10-16 22:48:07 +0530

    done

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com", "upload.wikimedia.org", "a0.muscache.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoic2hyZXlhbnNoMDE4IiwiYSI6ImNrdXR4Y2x3dDBvc3YycGx0MHVrdDE3Z2kifQ.He7GbS1jiCRaoEvRSTZpHg",
+  },
 };
